e7c2a20f78e86b5a7430ed3e38bcf2cade9bf893
majedalswaeer <majedalswaeer69@gmail.com>
2021-11-06 19:30:08 +0200

    first commit

---
+++ data_processing.py
@@ -0,0 +1,18 @@
+def get_data(facebook_page_name):
+    """
+    This Function gets a facebook page posts and likes 
+    
+    Args:
+        facebook_page_name: String
+    
+    Returns : Object containing latest facebook page posts
+    
+    
+    """
+    posts = {
+        "post_id":'12345',
+        "post_txt": 'katha katha katha',
+        "likes": 200
+        }
+    
+    return posts
